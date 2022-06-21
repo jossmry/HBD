4d4aac65b7015ef1767310c02cb9820d6135686b
jossmry <107823746+jossmry@users.noreply.github.com>
2022-06-21 00:49:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector("#yesBtn");
 
 yesBtn.addEventListener("click",function() {
-  alert("¡Hola! mira lo que voy aprendiendo jiji ¡Feliz Cumpleaños!🥳🎉🎁 (bueno, ambos sabemos que esto lo estas leyendo 3 días después🥴) Hey hey!... espero no hayas estado intentando clickear el otro botón😾💔 En verdad, ya sabía tu respuesta🤍... Aclaremos que yo te quiero mucho más🥰 Ahora, luego de este gran mensaje de alerta es hora de escanear el código QR Azul👀");
+  alert("¡Hola! mira lo que voy aprendiendo jiji ¡Feliz Cumpleaños!🥳🎉🎁 (bueno, ambos sabemos que esto lo estas leyendo 3 días después de tu cumple🥴) Hey hey!... espero no hayas estado intentando clickear el otro botón😾💔 En verdad, ya sabía tu respuesta🤍 solo queria presumir mis conocimientos... no mentira, bromita. Oye oye oye oye Aclaremos que yo te quiero mucho más🥰!!! Ahora, luego de este gran mensaje de alerta para presumir mi cariño hacia ti, es hora de escanear el código QR Azul👀");
 });
 
 const noBtn = document.querySelector("#noBtn");
